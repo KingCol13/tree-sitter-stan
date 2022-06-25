f1cd776467e8cfde0f1e20cf676b884a107a1fd6
KingCol13 <kingsleydgs@gmail.com>
2022-06-25 02:55:33 +0100

    Types highlighting

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -38,22 +38,6 @@
 (integer_literal) @number
 (real_literal) @number
 
-(basic_type) @type
-(return_type) @type
-(int_type) @type
-(real_type) @type
-(vector_type) @type
-(ordered_type) @type
-(positive_ordered_type) @type
-(simplex_type) @type
-(unit_vector_type) @type
-(row_vector_type) @type
-(matrix_type) @type
-(cholesky_factor_cov_type) @type
-(cholesky_factor_corr_type) @type
-(cov_matrix_type) @type
-(corr_matrix_type) @type
-
 "int" @type
 "real" @type
 "vector" @type
